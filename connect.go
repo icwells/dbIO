53fcae2928297f22ca79daa5650818cc01cf00ab
Shawn Rupp <smrupp16@gmail.com>
2019-01-28 10:15:57 -0700

    Moved struct and connection functions to connect.go and replaced blank database with localhost

---
+++ connect.go
@@ -0,0 +1,101 @@
+// Defines DBIO struct and connection functions
+
+package dbIO
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/Songmu/prompter"
+	_ "github.com/go-sql-driver/mysql"
+	"os"
+	"time"
+)
+
+var (
+	LOCALHOST = "tcp(localhost:3306)"
+)
+
+type DBIO struct {
+	DB        *sql.DB
+	Database  string
+	User      string
+	Password  string
+	Starttime time.Time
+	Columns   map[string]string
+}
+
+func (d *DBIO) create(database string) {
+	// Creates new database with utf8 charset
+	cmd, err := d.DB.Prepare(fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s CHARACTER SET utf8mb4;", database))
+	if err != nil {
+		fmt.Printf("\t[Error] Formatting command to create database %s: %v\n", database, err)
+	} else {
+		_, err = cmd.Exec()
+		if err != nil {
+			fmt.Printf("\t[Error] Creating database %s: %v\n", database, err)
+		}
+	}
+}
+
+func CreateDatabase(database, user string) *DBIO {
+	// Connects and creates new database
+	d := Connect(LOCALHOST, user)
+	d.create(database)
+	// Return conneciton to given database
+	d.Database = database
+	d.connect()
+	return d
+}
+
+func ReplaceDatabase(database, user string) *DBIO {
+	// Deletes database and creates new one (for testing)
+	d := Connect(LOCALHOST, user)
+	cmd, err := d.DB.Prepare(fmt.Sprintf("DROP DATABASE IF EXISTS %s;", database))
+	if err != nil {
+		fmt.Printf("\t[Error] Formatting command to delete database %s: %v\n", database, err)
+	} else {
+		_, err = cmd.Exec()
+		if err != nil {
+			fmt.Printf("\t[Error] Deleting database %s: %v\n", database, err)
+		} else {
+			d.create(database)
+		}
+	}
+	// Return dcnneciton to given database
+	d.Database = database
+	d.connect()
+	return d
+}
+
+func (d *DBIO) connect() {
+	// Connects to database
+	var err error
+	if d.User != "guest" && len(d.Password) < 1 {
+		// Prompt for password
+		d.Password = prompter.Password("\n\tEnter MySQL password")
+	}
+	// Begin recording time after password input
+	d.Starttime = time.Now()
+	cmd := d.User + ":" + d.Password
+	if len(d.Database) > 0 {
+		// Connect to specific database
+		cmd = cmd + "@/" + d.Database + "?charset=utf8mb4"
+	}
+	d.DB, err = sql.Open("mysql", cmd)
+	if err != nil {
+		fmt.Printf("\n\t[Error] Incorrect username or password: %v", err)
+		os.Exit(1000)
+	}
+	if err = d.DB.Ping(); err != nil {
+		fmt.Printf("\n\t[Error] Cannot connect to database: %v", err)
+	}
+}
+
+func Connect(database, user string) *DBIO {
+	// Attempts to connect to sql database. Returns dbio instance.
+	d := new(DBIO)
+	d.Database = database
+	d.User = user
+	d.connect()
+	return d
+}
